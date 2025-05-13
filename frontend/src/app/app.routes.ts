e4c864758a46bb8498d2617dee24fd55055348e9
Fenosoa <fenochoumadone@gmail.com>
2025-05-13 15:54:54 +0200

    third commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
 import { ProductsComponent } from './components/products/products/products.component';
+import { BagComponent } from './components/bag/bag/bag.component';
 
 export const routes: Routes = [
     {
         // path:'',component:HomeComponent
-        path:'',component:ProductsComponent
+        // path:'',component:ProductsComponent
+        path:'',component:BagComponent
     }
 ];
